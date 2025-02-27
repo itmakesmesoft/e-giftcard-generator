219d4afbc3b271d516804fe952b28eac0dc15806
Daniel LEE <dmsgur7112@gmail.com>
2025-02-27 22:33:43 +0900

    feat: 프로젝트 생성

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Gifticon Generator",
+  description: "누구나 쉽게 기프티콘을 생성할 수 있어요.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
